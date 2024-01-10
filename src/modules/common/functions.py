3895da8a91d0e2d6703159506ef7b6169d096c3f
djerfy <djerfy@gmail.com>
2024-01-10 14:06:55 +0100

    misc: remove empty line
    Signed-off-by: djerfy <djerfy@gmail.com>

--- src/modules/common/functions.py
+++ src/modules/common/functions.py
@@ -9,7 +9,7 @@ def matchLabels(match_labels=None, object_labels=None):
     for i in [match_labels, object_labels]:
         if i is None or i == [] or i == "" or i == "*":
             return False
-        
+
     object_labels = str(object_labels).replace("{", "").replace("}", "").replace("'", "").replace(" ", "").split(",")
 
     for object_label in object_labels:
@@ -18,5 +18,5 @@ def matchLabels(match_labels=None, object_labels=None):
             for separator in ["=", ":"]:
                 if match_label.split(separator)[0] == key and match_label.split(separator)[1] == value:
                     return True
-            
+
     return False
